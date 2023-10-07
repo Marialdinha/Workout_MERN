49a5484e885604b319c7c4eb360f4cad6dd9e172
Marialdinha <marialda@bellsouth.net>
2023-10-07 15:16:37 -0500

    Run npm install, created packge-lock.json

---
+++ client/src/main.jsx
@@ -0,0 +1,28 @@
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Error from './pages/Error'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/profiles/:profileId',
+        element: <Profile />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
